ade2decb2400b910ad9457ea9c4685eaefa8ed57
GasparSosnowski <janjanagoni@gmail.com>
2021-12-30 11:53:53 +0100

    details fragment issue fixed

--- app/src/main/java/com/example/schema/data/models/Currency.kt
+++ app/src/main/java/com/example/schema/data/models/Currency.kt
@@ -1,6 +1,10 @@
 package com.example.schema.data.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Currency(
     val name : String,
     val rate : String
-)
+) : Parcelable
